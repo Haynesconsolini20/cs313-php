bb86e369140ec5f78c36adda70b7ba04d2aa12e0
Sam Haynes <haynesconsolini@gmail.com>
2019-04-30 14:41:54 -0700

    footer changes

--- web/footer.php
+++ web/footer.php
@@ -1,12 +1,9 @@
-<?php 
-echo
-'<div class="footer">
+<div class="footer">
       Last edited:
       <?php
         putenv("TZ=America/Los_Angeles");
         echo date("m/d/y", filemtime($_SERVER["SCRIPT_FILENAME"]));
       ?>
     </div>
-</body>';
-?>
+</body>
 
