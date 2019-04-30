1a43d4f939ad9b39b9095f3783a02c7642eef66f
Sam Haynes <haynesconsolini@gmail.com>
2019-04-30 13:07:56 -0700

    first draft templating

---
+++ web/footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+      Last edited:
+      <?php
+        putenv("TZ=America/Los_Angeles");
+        echo date("m/d/y", filemtime($_SERVER["SCRIPT_FILENAME"]));
+      ?>
+    </div>
+</body>
---
+++ web/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="sidebar">
+    <a href="index.php"><img src="images/snare.jpg" id="profile" /></a>
+    <a href="index.php">Home</a>
+    <a href="assignments.php">Assignments</a>
+  </div>
