525a09d8646f6ac5063c12cdc84187c1cc605470
Sam Haynes <haynesconsolini@gmail.com>
2019-05-27 17:13:15 -0700

    js fixes

--- web/placer/members.php
+++ web/placer/members.php
@@ -15,9 +15,18 @@
     <div class="top">
     </div>
     <div class="content">
-        <h1>Parents</h1>
+<?php 
+session_start();
+if ($_SESSION['logged_in'] == true) {
+    echo '<h1>Logged in</h1>';
+}
+else {
+echo        
+        '<h1>Parents</h1>
         <p>Members</p>
         Username: <input type="text" name="username"><br>
         Password: <input type="text" name="password"><br>
-        <button id="submit_qry">Submit</button>
-    </div>
+        <button id="submit_qry">Submit</button>'
+}
+?>
+</div>
