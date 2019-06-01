cbb1454e1e777704e56b316192e80d48d6a3548d
Sam Haynes <haynesconsolini@gmail.com>
2019-06-01 14:32:41 -0700

    added join page

--- web/placer/query.php
+++ web/placer/query.php
@@ -101,7 +101,7 @@ else if ($_POST['type'] == 'register') {
   (id,first_name,last_name,role_id,username,user_password)
   VALUES
   (
-  nextval(\'users_id_s1\'),\'".
+  nextval('users_id_s1'),\'".
   $_POST['first_name']."\',\'".
   $_POST['last_name']."\',
   (SELECT id FROM roles WHERE role_desc = \'".$_POST['role']."\'),\'".
