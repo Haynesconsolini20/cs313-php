223f5cbd2d4527d3c4f0efcbdcdcfc5f1dd615f5
Sam Haynes <haynesconsolini@gmail.com>
2019-06-03 15:46:07 -0700

    Added role changing

--- web/placer/staff.php
+++ web/placer/staff.php
@@ -25,6 +25,50 @@ else if ($_SESSION['role'] == 'Staff') {
             <option value="Bass">Basses</option>
         </select>
         <div id="section_list"></div>';
+    echo 
+        '<h1>Assign sections</h1>
+        <p>This is a list of accounts without section assignments, you may assign them a section here.</p>';
+    try
+    {
+        $dbUrl = getenv('DATABASE_URL');
+    
+        $dbOpts = parse_url($dbUrl);
+    
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+    
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+    }
+    $students = array();
+    $section_query = $db->query("SELECT id, first_name, last_name FROM users WHERE role_id = (SELECT id FROM roles WHERE role_desc = 'Member') AND instrument_id is null");
+    echo "<table>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Section</th>
+            </tr>";
+    foreach($section_query as $row) {
+        echo "<tr>
+                <td>".$row['first_name']."</td>
+                <td>".$row['last_name']."</td>
+                <select class=change_section id=".$row['id'].">
+                    <option value=none>-----</option>
+                    <option value=Snare>Snare</option>
+                    <option value=Tenor>Tenor</option>
+                    <option value=Bass>Bass</option>
+                </select>";
+    }
+
 }
 else {
     echo '<h1>You do not have permission to access this page</h1>';
